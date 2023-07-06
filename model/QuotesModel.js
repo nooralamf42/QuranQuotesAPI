cbf7cdfaab232118d31eb9ec4b4e3082ebc67207
Noor Alam <90553481+nooralamf42@users.noreply.github.com>
2023-07-06 16:02:36 +0530

    Update QuotesModel.js

--- model/QuotesModel.js
+++ model/QuotesModel.js
@@ -3,7 +3,7 @@ export const myRouter = Router()
 import { getAllQuotes, addQuote, addQuotes, getQuote } from "../controller/QuotesController.js";
 
 myRouter
-        .get("/", getAllQuotes)
-        .get("/:quoteIndex", getQuote)
+        .get("/getAll", getAllQuotes)
+        .get("/get/:quoteIndex", getQuote)
         .post("/addQuote", addQuote)
         .post("/addQuotes", addQuotes)
